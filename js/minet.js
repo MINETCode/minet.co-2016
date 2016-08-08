fa33378b6058f67a4e8edf652d11089f6e3c5466
Shreyvardhan Sharma <mail@shrey.co>
2016-08-09 00:24:01 +0530

    Added toggle functionality

--- js/minet.js
+++ js/minet.js
@@ -1,3 +1,19 @@
+$("body#schedule #schedule-table .toggle-btn").click(function() {
+
+  if ($(this).hasClass("selected")) {
+    //do nothing lol
+  } else {
+    $(".toggle-btn.selected").removeClass("selected");
+    $(this).addClass("selected");
+  }
+
+  var dayInfo = $(this).attr('id');
+  var tableid = "#" + dayInfo;
+  $("#schedule-table table.selected").removeClass("selected");
+  $("#schedule-table").find("table" + tableid).addClass("selected");
+
+});
+
 var startDate = new Date(2016, 7, 13, 21, 41, 00);
 var currentDate = new Date();
 var interval = (startDate - currentDate);
